edcb301a97221e3190691841b983035fba42b57f
Brittany Chiang <brittany.chiang@gmail.com>
2022-08-09 11:09:31 -0400

    Add php codesniffer and lint

--- filters.php
+++ filters.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Description:     Exposes filters
+ *
+ * @package         UpsAlgolia
+ */
+
 namespace UpsAlgolia\Filters;
 
 use UpsAlgolia\Utils;
@@ -7,23 +13,23 @@ use \Exception as Exception;
 /**
  * Does this post need to be indexed?
  *
- * @param string $id    id of post
- * @param object $post  post
+ * @param string $id    ID of post.
+ * @param object $post  WP post.
  *
  * @return bool
  */
-function is_indexable($id, $post) {
-  if (has_filter('UpsAlgolia\is_indexable')) {
-    return apply_filters('UpsAlgolia\is_indexable', $id, $post);
-  }
+function is_indexable( $id, $post ) {
+	if ( has_filter( 'UpsAlgolia\is_indexable' ) ) {
+		return apply_filters( 'UpsAlgolia\is_indexable', $id, $post );
+	}
 
-  $post_status = $post->post_status;
+	$post_status = $post->post_status;
 
-  // Only reindex posts that have been published or trashed
-  $is_valid_status = $post_status === 'publish' || $post_status === 'trash';
-  $revision_or_autosave = wp_is_post_revision($id) || wp_is_post_autosave($id);
+	// Only reindex posts that have been published or trashed.
+	$is_valid_status      = 'publish' === $post_status || 'trash' === $post_status;
+	$revision_or_autosave = wp_is_post_revision( $id ) || wp_is_post_autosave( $id );
 
-  return $is_valid_status && !$revision_or_autosave;
+	return $is_valid_status && ! $revision_or_autosave;
 }
 
 /**
@@ -32,67 +38,67 @@ function is_indexable($id, $post) {
  * @return object { "application_id": <Algolia application id>, "admin_key": <Algolia admin key> }
  */
 function get_algolia_application() {
-  return apply('UpsAlgolia\get_algolia_application', null);
+	return apply( 'UpsAlgolia\get_algolia_application', null );
 }
 
 
 /**
  * Get Algolia index name.
  *
- * @param object $post post
+ * @param object $post WP post.
  *
  * @return string
  */
-function get_index_name($post) {
-  return apply('UpsAlgolia\get_index_name', $post);
+function get_index_name( $post ) {
+	return apply( 'UpsAlgolia\get_index_name', $post );
 }
 
 
 /**
  * Get Algolia settings.
  *
- * @param string $index index name
+ * @param string $index Index name.
  *
  * @return object
  */
-function get_algolia_settings($index) {
-  return apply('UpsAlgolia\get_algolia_settings', $index);
+function get_algolia_settings( $index ) {
+	return apply( 'UpsAlgolia\get_algolia_settings', $index );
 }
 
 /**
  * Get Algolia rules.
  *
- * @param string $index index name
+ * @param string $index Index name.
  *
  * @return object
  */
-function get_algolia_rules($index) {
-  return apply('UpsAlgolia\get_algolia_rules', $index);
+function get_algolia_rules( $index ) {
+	return apply( 'UpsAlgolia\get_algolia_rules', $index );
 }
 
 /**
  * Get Algolia synonyms.
  *
- * @param string $index index name
+ * @param string $index Index name.
  *
  * @return object
  */
-function get_algolia_synonyms($index) {
-  return apply('UpsAlgolia\get_algolia_synonyms', $index);
+function get_algolia_synonyms( $index ) {
+	return apply( 'UpsAlgolia\get_algolia_synonyms', $index );
 }
 
 /**
  * Apply the given filter with the arguments
  *
- * @param string $filter filter name
- * @param array  $args   arguments to be passed to filter
+ * @param string $filter Filter name.
+ * @param array  ...$args Arguments to be passed to filter.
  *
  * @return mixed
  */
-function apply($filter, ...$args) {
-  if (has_filter($filter)) {
-    return apply_filters($filter, ...$args);
-  }
+function apply( $filter, ...$args ) {
+	if ( has_filter( $filter ) ) {
+		return apply_filters( $filter, ...$args );
+	}
 
-  Utils\throw_undefined_filter_exception($filter);
+	Utils\throw_undefined_filter_exception( $filter );
 }
--- utils.php
+++ utils.php
@@ -1,105 +1,111 @@
 <?php
+/**
+ * Description:     Utility functions for plugin
+ *
+ * @package         UpsAlgolia
+ */
+
 namespace UpsAlgolia\Utils;
 
 /**
  * Transform type to function friendly format.
  * - Replace '-' with '_'
  *
- * @param string $type type
+ * @param string $type Type.
  *
  * @return string
  */
-function transform_type($type) {
-  return str_replace('-', '_', $type);
+function transform_type( $type ) {
+	return str_replace( '-', '_', $type );
 }
 
 /**
  * Get the serializer filter name.
  *
- * @param string $post_type post type
+ * @param string $post_type Post type.
  *
  * @return string e.g. UpsAlgolia\post_to_record
  */
-function get_serializer_filter($post_type) {
- return "UpsAlgolia\\${post_type}_to_record";
+function get_serializer_filter( $post_type ) {
+	return "UpsAlgolia\\${post_type}_to_record";
 }
 
 
 /**
  * Get all unique values for given key.
  *
- * @param object[] $arr array of objects
- * @param string   $key object key
+ * @param object[] $arr Array of objects.
+ * @param string   $key Object key.
  *
  * @return mixed[] all unique values
  */
-function get_unique_key_values($arr, $key) {
-  $values = array_column($arr, $key);
-  return array_unique($values);
+function get_unique_key_values( $arr, $key ) {
+	$values = array_column( $arr, $key );
+	return array_unique( $values );
 }
 
 /**
  * Create Algolia filter.
  *
- * @param string $attribute
- * @param string $value
+ * @param string $attribute Algolia attribute.
+ * @param string $value Value for attribute.
  *
  * @return string e.g. type:"page"
  */
-function create_filter($attribute, $value) {
-  return "${attribute}:\"${value}\"";
+function create_filter( $attribute, $value ) {
+	return "${attribute}:\"${value}\"";
 }
 
 /**
  * Create Algolia filters by chaining attribute:value with
  * given operator.
  *
- * @param string   $attribute attribute name
- * @param string[] $values    all values
- * @param string   $operator  operator between each filter
+ * @param string   $attribute Attribute name.
+ * @param string[] $values    All values.
+ * @param string   $operator  Operator between each filter.
  *
  * @return string  e.g. category:"cars" OR category:"bikes"
  */
-function chain_filters($attribute, $values, $operator = 'OR') {
-  $attributed_values = array_map(
-    function ($value) use ($attribute) {
-      return create_filter($attribute, $value);
-    },
-    $values
-  );
+function chain_filters( $attribute, $values, $operator = 'OR' ) {
+	$attributed_values = array_map(
+		function ( $value ) use ( $attribute ) {
+			return create_filter( $attribute, $value );
+		},
+		$values
+	);
 
-  return implode(" ${operator} ", $attributed_values);
+	return implode( " ${operator} ", $attributed_values );
 }
 
 
 /**
  * Map given key value pairs into Algolia filters.
  *
- * @param object $dict
- * @param string $operator
+ * @param object $dict Key value pairs.
+ * @param string $operator AND or OR operator.
  *
  * @return string e.g. type:"page" OR title:"UpsAlgolia"
  */
-function map_into_filters($dict, $operator = 'OR') {
-  $attributed_values = array_map(
-    function ($key) use ($dict) {
-      return create_filter($key, $dict[$key]);
-    },
-    array_keys($dict)
-  );
+function map_into_filters( $dict, $operator = 'OR' ) {
+	$attributed_values = array_map(
+		function ( $key ) use ( $dict ) {
+			return create_filter( $key, $dict[ $key ] );
+		},
+		array_keys( $dict )
+	);
 
-  return implode(" ${operator} ", $attributed_values);
+	return implode( " ${operator} ", $attributed_values );
 }
 
 /**
  * Throw exception for undefined filter.
  *
- * @param string filter
+ * @param string $filter The given filter.
  *
- * @throw Exception
+ * @throws \Error Error message.
  */
-function throw_undefined_filter_exception($filter) {
-  throw new \Error("Please define the ${filter} filter in your theme");
+function throw_undefined_filter_exception( $filter ) {
+	throw new \Error( "Please define the ${filter} filter in your theme" );
 }
 
 /**
@@ -108,10 +114,10 @@ function throw_undefined_filter_exception($filter) {
  * @return array
  */
 function get_searchable_post_types() {
-  return get_post_types(
-    array(
-      'public' => true,
-      'exclude_from_search' => false
-    )
-  );
+	return get_post_types(
+		array(
+			'public' => true,
+			'exclude_from_search' => false,
+		)
+	);
 }
